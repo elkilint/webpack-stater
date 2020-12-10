95810a61b427b7622e24010f9d35b67a106c6326
Elkin <elkilint@gmail.com>
2020-12-09 19:55:30 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import { saludar } from './js/componentes.js';
+
+import {Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 import './styles.css';
+export const todoList = new TodoList();
+todoList.todos.forEach(todo => crearTodoHtml(todo ));
 
 
 
-const nombre = 'a la nube';
+//const tarea = new Todo('Viajar');
 
-saludar( nombre );
+//todoList.nuevoTodo (tarea);
+
+//console.log(todoList);
+//crearTodoHtml(tarea);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,14 +1,130 @@
-import '../css/componentes.css';
+
+    //Referencias HTML
+
+import { Todo } from "../classes";
+import {todoList } from "../index";
+const divTodoList = document.querySelector('.todo-list');
+const textInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFilters = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
+
+export const crearTodoHtml = (todo) => {
+
+const htmlTodo =
+
+`
+<li class="${ (todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+	<div class="view">
+		<input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : ''}>
+		<label>${todo.tarea} </label>
+		<button class="destroy"></button>
+	</div>
+    <input class="edit" value="Create a TodoMVC template">
+</li>`	
+
+const div = document.createElement('div');
+
+div.innerHTML = htmlTodo;
+divTodoList.append (div.firstElementChild);
+return div.firstElementChild;
+
+} 
+
+//Eventos
+
+textInput.addEventListener('keyup', (event) => {
+
+  if(event.keyCode === 13 && textInput.value.length > 0 ){
+
+        console.log(textInput.value);
+        const nuevoTodo  = new Todo(textInput.value);
+        todoList.nuevoTodo (nuevoTodo);
+        console.log(todoList); 
+        crearTodoHtml(nuevoTodo);
+        textInput.value = '';
+    }
+});
+
+// evento de marcar complatada una tarea
+
+divTodoList.addEventListener('click', (event) => {
+
+
+const nombreElemento = event.target.localName; //label, boton, input
+const todoElemento   = event.target.parentElement.parentElement
+const todoId         = todoElemento.getAttribute('data-id');
 
 
 
-export const saludar = ( nombre ) => {
+if (nombreElemento.includes('input')) { // se hizo click en el check?
 
-    console.log('Creando etiqueta h1, en el HTML!');
+todoList.marcarCompletado (todoId);
+todoElemento.classList.toggle('completed');
+}
+else if (nombreElemento.includes('button')) { //borrar todo
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Repositorio de aplicaciones  ${ nombre }`;
-
-    document.body.append( h1 );
+    todoList.eliminarCompletado (todoId);
+    divTodoList.removeChild (todoElemento);
 
 }
+
+});
+
+btnBorrar.addEventListener('click', () => {
+
+    todoList.eliminarCompletado ();
+
+    for (let i = divTodoList.children.length-1; i>=0; i-- ) {
+
+        const elemento = divTodoList.children [i];
+        console.log (elemento);
+
+        if (elemento.classList.contains('completed')){
+
+          divTodoList.removeChild (elemento);  
+        }
+                
+    }
+})
+
+ulFilters.addEventListener('click', (event) => {
+
+    const filtro =  (event.target.text);
+    if (!filtro){
+        return; }
+
+    anchorFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+
+    for(const elemento of divTodoList.children){
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+
+            case 'Pendientes' :
+                if(completado) {
+                    elemento.classList.add('hidden');
+
+                }
+                break;
+
+            case 'Completados' :
+
+            if(!completado) {
+                elemento.classList.add('hidden');
+
+            }
+            break;
+
+        }
+
+
+    }
+
+
+        
+    });
